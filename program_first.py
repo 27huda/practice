88eb96bf58dd4f2b231febc693f6776ac3d2a154
27huda <162413521+27huda@users.noreply.github.com>
2024-03-06 19:01:22 +0530

    initial commit

---
+++ program_first.py
@@ -0,0 +1,11 @@
+class SumOfTwoNumbers():
+    def addition(self,a,b):
+        sum = a+b
+        print("the sum is ",sum)
+
+if __name__ == "__main__":
+    a = 2
+    b=3
+    obj = SumOfTwoNumbers()
+    obj.addition(a,b)
+
